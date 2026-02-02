21f2c71bbbe5c679a2cc2621f23a72f8ba84a6a5
khy13 <kihy0103@gmail.com>
2026-02-02 13:31:24 +0900

    folder, script 및 scene 일부 생성
    scene
    - Title, Village, Dungeon
    
    script
    - GameManager, SoundManager, MySceneManager, SaveLoadManager, UIManager(Title, Village, Dungeon)
    
    folder
    - Reading Strike 등

---
+++ Assets/Reading Strike/Scripts/Manager/MySceneManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ReadingStrike.Manager
+{
+    public enum SceneType
+    {
+        Title,
+        Village,
+        Dungeon
+    }
+    public class MySceneManager : MonoBehaviour
+    {
+        public bool isSceneChanging;
+        public SceneType sceneType;
+        void Awake()
+        {
+            sceneType = SceneType.Title;
+        }
+        public void SceneChange(int index)
+        {
+            if (!IsSceneChangePossible(index)) return;
+            isSceneChanging = true;
+            SceneManager.LoadScene(index);
+            sceneType = (SceneType)index;
+            isSceneChanging = false;
+        }
+        public void SceneChangeStartCo(int index)
+        {
+            StartCoroutine(SceneChangeCo(index));
+        }
+        IEnumerator SceneChangeCo(int index)
+        {
+            if (!IsSceneChangePossible(index)) yield break;
+            isSceneChanging = true;
+            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(index);
+            while (!asyncLoad.isDone) yield return null;
+
+            isSceneChanging = false;
+        }
+        bool IsSceneChangePossible(int index)
+        {
+            if (isSceneChanging)
+            {
+                Debug.Log("Scene 변경중");
+                return false;
+            }
+            else if (SceneManager.GetActiveScene().buildIndex == index)
+            {
+                Debug.Log("같은 Scene으로 이동 불가");
+                return false;
+            }
+            else
+                return true;
+        }
+    }
+}
---
+++ Assets/Reading Strike/Scripts/Manager/UIManager/TitleUIManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ReadingStrike.Manager.UI
+{
+    public class TitleUIManager : UIManager
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+        public override void BtnSelect(int index)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void BtnSetting()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
